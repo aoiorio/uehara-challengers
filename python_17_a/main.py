19fee5843c37542d8e6c769830c23e0adb6198fd
aoiorio <130330538+aoiorio@users.noreply.github.com>
2023-07-08 19:35:34 +0900

    python_17を解きました

--- python_17_a/main.py
+++ python_17_a/main.py
@@ -2,14 +2,32 @@ marks = ["Spade", "Heart", "Diamond", "Club"]
 numbers = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"]
 cards = {}
 
-for number in numbers:
-    for mark in marks:
-        cards.setdefault(number, mark) # todo [mark]を指定すると、ずっと同じmarkになってしまうので、次の値が新しいkeyで更新されない
-print(cards) # cardでも同じ
+# for number in numbers:
+#     for mark in marks:
+#         cards[number] = mark # todo [mark]を指定すると、ずっと同じmarkになってしまうので、次の値が新しいkeyで更新されない
+# # print(cards) # cardでも同じ
 
 # cards = {shape: mark for shape, mark in zip(marks, numbers)}
 
 class Card:
-    ranks = {}
+    
+    def __init__(self, ranks):
+        self.__ranks = ranks
     def dic(self):
-        print("some stuff")
+        for m in marks:
+            for n in numbers:
+                if n == "A":
+                    self.__ranks['1'] = m
+                elif n == "J" or n == "Q" or n == "K":
+                    self.__ranks[10] = m
+                else:
+                    self.__ranks[n] = m
+        
+
+    def _get_value(self):
+        return self.__ranks
+    
+
+card = Card(cards)
+card.dic()
+print(card._get_value())
